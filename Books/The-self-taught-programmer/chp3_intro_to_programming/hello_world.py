f67dfe34eb298d044006a30789b08f29c7058ccd
Varenya Visen <varenyavisen@gmail.com>
2026-04-07 16:27:26 +0530

    KNN algorithm

---
+++ Books/The-self-taught-programmer/chp3_intro_to_programming/hello_world.py
@@ -0,0 +1,9 @@
+# Our first program - hello world 
+# Print it 100 times
+for i in range(100):
+    print("Hello World!",i)
+
+
+
+# This is a comment - we can write comment using `#` sign
+print("The comment does not affect the this code ")
